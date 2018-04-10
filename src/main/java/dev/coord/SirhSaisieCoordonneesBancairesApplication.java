650decf9338a2c5970c684c13d95ec34c0e20e00
Axel Buteau <buteauaxel@gmail.com>
2018-04-10 17:27:33 +0200

    init

---
+++ src/main/java/dev/coord/SirhSaisieCoordonneesBancairesApplication.java
@@ -0,0 +1,12 @@
+package dev.coord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirhSaisieCoordonneesBancairesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SirhSaisieCoordonneesBancairesApplication.class, args);
+	}
+}
